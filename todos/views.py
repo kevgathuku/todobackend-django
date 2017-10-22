734447276b00aa27a11fcbda92cb376e99029bc6
Kevin Ndung'u <kevgathuku@gmail.com>
2017-10-22 20:45:48 +0300

    Add POST request handling for creating todos

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.parsers import JSONParser
 
 from .models import Todo
 from .serializers import TodoSerializer
 
 
+@csrf_exempt
 def index(request):
     """
     List all todos, or create a new todo.
@@ -12,3 +16,11 @@ def index(request):
         todos = Todo.objects.all()
         serializer = TodoSerializer(todos, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
