601f9eced3f806c59001350692a759d2323a365a
Kevin Ndung'u <kevgathuku@gmail.com>
2017-10-22 20:07:35 +0300

    Add initial Todo model and migrations

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=256)
+    completed = models.BooleanField(default=False)
+    url = models.URLField(blank=True, default='')
