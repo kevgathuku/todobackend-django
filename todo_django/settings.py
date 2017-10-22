d9cf48404d7bdc97638c36f464b9b315367ee247
Kevin Ndung'u <kevgathuku@gmail.com>
2017-10-22 20:23:11 +0300

    Add simpler CORS settings

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,6 +127,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # CORS
-CORS_ORIGIN_WHITELIST = (
-    'todobackend.com',
-)
+CORS_ORIGIN_ALLOW_ALL = True
