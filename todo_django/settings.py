8c134d61d340abfe19b2348d2ed36df8d512b5bc
Kevin Ndung'u <kevgathuku@gmail.com>
2017-10-22 19:28:06 +0300

    Configure DB URL

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,10 +76,8 @@ WSGI_APPLICATION = 'todo_django.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':  dj_database_url.config(
+        default='postgres://kevin:@127.0.0.1:5432/todo_django_backend_dev')
 }
 
 
