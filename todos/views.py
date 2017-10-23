8d0ab683cd821111ab193b1db2f86631310f3ab6
Kevin Ndung'u <kevgathuku@gmail.com>
2017-10-23 14:52:24 +0300

    Implement individual todo_detail view

--- todos/views.py
+++ todos/views.py
@@ -29,3 +29,29 @@ def index(request):
     elif request.method == 'DELETE':
         Todo.objects.all().delete()
         return Response([])
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def todo_detail(request, todo_id):
+    """
+    Retrieve, update or delete a todo item.
+    """
+    try:
+        todo = Todo.objects.get(pk=todo_id)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
